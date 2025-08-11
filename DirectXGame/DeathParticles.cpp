5f6f9fa69c59407a7235e47308271c92c480b0af
NeumanYoshinori <k024g0084@g.neec.ac.jp>
2025-08-12 03:31:34 +0900

    フェードアウトを修正

--- DirectXGame/DeathParticles.cpp
+++ DirectXGame/DeathParticles.cpp
@@ -53,7 +53,7 @@ void DeathParticles::Update() {
 		isFinished_ = true;
 	}
 
-	color_.w = std::clamp(counter_, 0.0f, 1.0f);
+	color_.w = std::clamp(1.0f - counter_ / kDuration, 0.0f, 1.0f);
 	// 色変更オブジェクトに色の数値を設定する
 	objectColor_.SetColor(color_);
 
